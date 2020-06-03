8d5856a4081c0be3d0489b3c378e5777b39619e9
Jun Sok Huhh <anarinsk@gmail.com>
2020-06-03 09:12:28 +0900

    Fix some error
    - Not work properly
    - See `setup-korean-sh.ipynb` for the details
    - NOT SOLVED: Why py script should be run twice?

--- nanum.sh
+++ nanum.sh
@@ -4,3 +4,6 @@ sudo sed -i 's/archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 sudo apt-get update 
 sudo apt-get install fonts-nanum*
 sudo fc-cache -fv
+
+echo "Nanum fonts are set!" &&
+echo "-----------------------"
