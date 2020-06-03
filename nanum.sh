df8be0fb50a471f296614828018f7a0c2e8d7480
Jun Sok Huhh <anarinsk@gmail.com>
2020-06-03 16:41:59 +0900

    ad-hoc solution for kor font loading
    - Solution is not so clean, but works

--- nanum.sh
+++ nanum.sh
@@ -4,6 +4,10 @@ sudo sed -i 's/archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 sudo apt-get update 
 sudo apt-get install fonts-nanum*
 sudo fc-cache -fv
+python setting4pltfont.py
 
-echo "Nanum fonts are set!" &&
+#cp /usr/share/fonts/truetype/nanum/Nanum* /opt/conda/lib/python3.7/site-packages/matplotlib/mpl-data/fonts/ttf/
+#echo "font.family : NanumGothicCoding" >> /opt/conda/lib/python3.7/site-packages/matplotlib/mpl-data/matplotlibrc
+echo "1. Nanum fonts are set!" &&
+#echo "2. Append font.family to matplotrc"
 echo "-----------------------"
--- setting4pltfont.py
+++ setting4pltfont.py
@@ -1,17 +1,11 @@
-from matplotlib import font_manager, rc
-import matplotlib as mpl
+import matplotlib 
 import matplotlib.pyplot as plt 
-
-mpl.font_manager._rebuild()
-
-font_fname = '/usr/share/fonts/truetype/nanum/NanumGothicCoding.ttf'
-font_name = font_manager.FontProperties(fname=font_fname).get_name()
-
-print(f'{font_name} is set!')
-
+matplotlib.font_manager._rebuild()
+font_name = 'NanumGothicCoding'
 #plt.rcParams['figure.figsize'] = [15, 7]
-mpl.rcParams['axes.unicode_minus'] = False
+matplotlib.rcParams['axes.unicode_minus'] = False
 plt.rcParams["font.size"] = 10
 plt.rcParams['xtick.labelsize'] = 8
 plt.rcParams['ytick.labelsize'] = 8
 plt.rcParams['font.family'] = font_name
+print(f'{font_name} is set!')
